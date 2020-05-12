ff6a494a1d4f52b11b3a7df49f859ba0547ac946
多岐正行 <tagimasayuki@MacBook-Pro.local>
2020-05-12 10:23:36 +0900

    first

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="headderSection">
+    <Header />
+    <div class="logoParent">
+      <img class="pageLogo" src="../assets/logo.png" alt="ロゴ">
+    </div>
+    <div class="userName_authorityLevel">
+      <div class="userName">
+        小澤こうた
+      </div>
+      <div class="authorityLevel">
+        一般ユーザー
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.headderSection{
+  height: 70px;
+  background-color: lightgrey;
+}
+.logoParent{
+  text-align: left;
+}
+.pageLogo{ /*これがロゴマークになる？？？*/
+  width: 50px;
+  margin: 13px 0px 0px 10px;
+}
+
+.userName_authorityLevel {
+  position: relative;
+  bottom: 64px;
+  padding: 0pt 15pt 0pt 0pt;
+  text-align: right;
+}
+
+.userName{
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin: 0pt 0pt 0pt 0pt;
+}
+
+.authorityLevel{
+  margin: 0pt 0pt 0pt 0pt;
+}
+
+</style>
+
+<!--ヘッダー-->
---
+++ vue/src/views/Result.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="resultSection">
+    <div>参照画面</div>
+    <Chart />
+  </div>
+</template>
+
+<script>
+
+import Chart from '../components/Chart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Chart
+  }
+}
+
+</script>
