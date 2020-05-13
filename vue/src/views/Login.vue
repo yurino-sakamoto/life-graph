7cd2ce63c4423e351b2e987be8831c688d3f3c98
多岐正行 <tagimasayuki@MacBook-Pro.local>
2020-05-13 16:18:02 +0900

    ログイン画面調整

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,11 +1,11 @@
-<!--【参考】https://www.alaxala.com/jp/techinfo/archive/manual/AX2400S/HTML/11_7/CFGUIDE2/0189.HTM-->
-
 <template>
   <div class="loginSection">
     <div class="pageTitle">
-      人生グラフ♪
+      人生グラフ
     </div>
+    <!--
     <img class="pageLogo" src="../assets/logo.png" alt="Logo">
+    -->
     <div class="loginID">
       <label for="password">username:
         <input type="text" name="uid" size="40" maxlength="32" autocomplete="OFF">
@@ -16,9 +16,11 @@
         <input type="password" name="pwd" size="40" maxlength="32" autocomplete="OFF">
       </label>
     </div>
-    <router-link to="/">
-      <input type="submit" value="Login">
-    </router-link>
+    <div class="loginButton">
+      <router-link to="/">
+        <input type="submit" value="Login">
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -34,7 +36,6 @@
 </script>
 
 <style scoped>
-
 .loginSection{
   height: 280px;
   background-color: blanchedalmond;
@@ -42,20 +43,20 @@
   margin: 100px;
 }
 
-.pageTitle{
-  padding: 50px 0px 0px 0px;
-  margin: 0px 0px 10px 0px;
+.loginButton{
 }
 
+.pageTitle{
+  font-size: 2rem;
+  padding: 50px 0px 0px 0px;
+  margin: 0px 0px 30px 0px;
+}
 .pageLogo{
   margin: 10px 0px 10px 0px;
   width: 50px;
 }
-
 .loginID,
 .loginPassword{
   margin: 10px 90px 10px 20px;
 }
 </style>
-
-<!--ログイン-->
