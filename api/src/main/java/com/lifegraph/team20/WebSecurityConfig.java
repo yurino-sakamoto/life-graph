b31c8ae5de1aba4135e416431b8d310551889142
yurino-sakamoto <yurino.sakamoto@seattleconsulting.co.jp>
2020-05-13 19:28:41 +0900

    fix

--- api/src/main/java/com/lifegraph/team20/WebSecurityConfig.java
+++ api/src/main/java/com/lifegraph/team20/WebSecurityConfig.java
@@ -11,5 +11,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().antMatchers("/auth/login**").permitAll().anyRequest().authenticated();
   }
-
 }
