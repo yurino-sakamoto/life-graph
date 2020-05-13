4000ff6f2e93944229e4bf1cf15b49ce40f7c2ed
keitayamaoka <62986434+keitayamaoka@users.noreply.github.com>
2020-05-13 23:54:40 +0900

    人生グラフ検索API修正分

---
+++ api/src/main/java/com/lifegraph/team20/search/SearchContoroller.java
@@ -0,0 +1,31 @@
+package com.lifegraph.team20.search;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+//サーチ情報 Controller
+@Controller
+public class SearchController {
+@Autowired
+private JdbcTemplate jdbcTemplate;
+public List<SearchCategory> selectSearchCategories() {
+    final String sql = "select * from parent_graphs";
+    return jdbcTemplate.query(sql, new RowMapper<SearchCategory>() {
+        public SearchCategory mapRow(ResultSet rs, int rowNum) throws SQLException{
+                return new se
+        }
+    }
+}
+    @GetMapping(value = "SearchList")
+    public String displayList(Model model) {
+        List<Search>
+    }
+//  @RequestMapping("/zipcode")
+//  public String zipcodeForm(HttpSession session, Model nodel) {
+//      return "zipcode";
+//  }
+//
+//  @RequestMapping(value="/")
+}
