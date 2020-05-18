f7cd85b547e8d6a9a88973c42baecc188d274f86
岡崎悠 <okazakiyu@okazakiyuunoMacBook-Air.local>
2020-05-18 17:31:37 +0900

    ログアウト兼繋ぎ込み

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -7,32 +7,54 @@
     <img class="pageLogo" src="../assets/logo.png" alt="Logo">
     -->
     <div class="loginID">
-      <label for="password">username:
-        <input type="text" name="uid" size="40" maxlength="32" autocomplete="OFF">
-      </label>
+      <!-- <label for="password">username:
+        <input type="text" name="uid" size="40" maxlength="32" autocomplete="OFF"> -->
+      <label for="username" />
+      <input v-model="username" type="username" name="username" placeholder="UserName">
+      <label for="password" />
+      <input v-model="password" type="password" name="password" placeholder="Password">
     </div>
-    <div class="loginPassword">
-      <label for="password">password:
-        <input type="password" name="pwd" size="40" maxlength="32" autocomplete="OFF">
-      </label>
-    </div>
-    <div class="loginButton">
+    <button class="button-panel" @click="login()">
+      Sign in
+    </button>
+    <!-- <div class="loginButton">
       <router-link to="/">
         <input type="submit" value="Login">
       </router-link>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-//
-// export default {
-//   data: {
-//     return{
-//       textInput:'aa',
-//     }
-//   }
-// }
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  computed: {
+    token () {
+      return this.$store.state.auth.token
+    }
+  },
+  watch: {
+    token (newToken) {
+      this.$router.push('/top')
+    }
+  },
+  methods: {
+    login () {
+      this.$store.dispatch(
+        'auth/create',
+        {
+          username: this.username,
+          password: this.password
+        }
+      )
+    }
+  }
+}
 </script>
 
 <style scoped>
