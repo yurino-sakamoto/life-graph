b1f3605ccf68acfb6092fd5411f2b8debeb6147d
岡崎悠 <okazakiyu@okazakiyuunoMacBook-Air.local>
2020-05-18 17:42:34 +0900

    a

--- vue/src/store/modules/chart.js
+++ vue/src/store/modules/chart.js
@@ -7,18 +7,18 @@ export default {
   namespaced: true,
 
   state: {
-    // 状態
-    count: 0
+    //状態
+    count:0
   },
   getters: {
-    // storeの状態を変更するメソッド(同期処理のみ)
+    //storeの状態を変更するメソッド(同期処理のみ)
 
   },
   mutation: {
 
   },
-  actions: {
-    // storeの状態を変更するメソッド(非同期処理も)
+  actions:{
+    //storeの状態を変更するメソッド(非同期処理も)
 
   }
 }
