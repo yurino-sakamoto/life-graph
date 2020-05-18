b75d67ad7479119da03ebb594cff3eb02756a37a
keitayamaoka <62986434+keitayamaoka@users.noreply.github.com>
2020-05-18 17:35:24 +0900

    人生グラフ検索API

--- api/src/main/java/com/lifegraph/team20/search/SearchRepository.java
+++ api/src/main/java/com/lifegraph/team20/search/SearchRepository.java
@@ -1,11 +1,8 @@
 //package com.lifegraph.team20.search;
-
-//public interface SearchRepository extends <Search, Integer> {
-//}
-//import org.springframework.
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.stereotype.Repository;
-//public interface SearchRepository enxtend
-//@RequestMapping(value = "/api/search" , method = RequestMethod.GET)
-//public String search(Locale locale, Model moderl) {
-//  logger.info("welcome SkillUpload! The client locale is {}", locale);
+//
+//@Repository
+//public interface SearchRepository extends JpaRepository <Search, String> {
 //}
