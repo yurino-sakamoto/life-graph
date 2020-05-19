e5d2033469873aa7d6f24032523f51c128c76378
多岐正行 <tagimasayuki@MacBook-Pro.local>
2020-05-19 18:46:52 +0900

    Header

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -29,10 +29,10 @@
         ユーザー名：{{ userName }}
       </div>
       <div class="authorityLevel">
-        <p v-if="authority=='general'">
+        <p v-if="authority=='1'">
           一般ユーザー
         </p>
-        <p v-else-if="authority=='admin'">
+        <p v-else-if="authority=='2'">
           管理者
         </p>
         <p v-else>
@@ -44,27 +44,31 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      userName: '小澤コウタ',
-      user: 'general',
-      authority: 'admin'
-    }
-  }
-  // computed: {
-  //   Name () {
-  //     const userName = //javaで取得する場所
-  //     return this.userName
-  //   }
-  // },
-  // computed: {
-  //   Authority () {
-  //     const authority = //javaで取得する場所
-  //     return this.authority
-  //   }
-  // }
-}
+// export default {
+//   data () {
+//     return {
+//       userName: '小澤コウタ',
+//       authority: '1'
+//     }
+//   },
+//   mounted () {  //ヘッダー表示と同時にdispatch
+//     this.$store.dispatch('accountAction');
+//   },
+//   computed: {
+//     Name () {
+//       this.userName = this.$store.getters.acountName
+//       return this.userName
+//     },
+//     Authority () {
+//       this.authority = this.$store.getters.acountAuthority
+//       return this.authority
+//     }
+//   }
+//   //   ,logout () {
+//   //     this.$store.commit('auth/deleteToken')  //authのstateのtokenを消す
+//   //     this.$router.push('/login')             //tokenが消されたあとログイン画面に遷移する
+//   // }
+// }
 </script>
 
 <style scoped>
